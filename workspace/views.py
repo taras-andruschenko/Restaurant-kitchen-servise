09364bc876cdce4d5750e045658fc1f5b388994e
Taras Andruschenko <And999110@gmail.com>
2022-11-10 16:25:29 +0200

    Create index view

--- workspace/views.py
+++ workspace/views.py
@@ -1,3 +1,28 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from workspace.models import Dish, DishType, Ingredient
+
+
+@login_required
+def index(request):
+    """View function for the home page of the site."""
+
+    num_cooks = get_user_model().objects.count()
+    num_dishes = Dish.objects.count()
+    num_dish_types = DishType.objects.count()
+    num_ingredients = Ingredient.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_dishes": num_dishes,
+        "num_dish_types": num_dish_types,
+        "num_ingredients": num_ingredients,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "workspace/index.html", context=context)
